15e9627301cea544867231cdec1de316d68049b9
edilane pinheiro <edilane.pinheiro@hotmail.com>
2023-03-06 20:21:15 -0400

    Enviando projeto

---
+++ aulaMarco/Filme.java
@@ -0,0 +1,16 @@
+package aulaMarco;
+
+class Filme {
+    private String titulo;
+    private int duracao;
+    private int ano;
+    private String sinopse;
+
+    public String getTitulacao() {
+        return this.titulo;
+    }
+
+    public void setTitulacao(String titulo) {
+        this.titulo = titulo;
+    }
+}
---
+++ aulaMarco/Principal.java
@@ -0,0 +1,9 @@
+package aulaMarco;
+
+public class Principal {
+    public static void main(String[] args) {
+        Filme filme1 = new Filme();
+        filme1.setTitulacao("Era do Gelo");
+        System.out.println(filme1.getTitulacao());
+    }   
+}
