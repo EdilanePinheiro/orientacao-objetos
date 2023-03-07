a67e6b9c0f06fa8cc00d337445272468a3027c9e
edilane pinheiro <edilane.pinheiro@hotmail.com>
2023-03-06 20:42:28 -0400

    Terminando de instaciar as classes

--- aulaMarco/Filme.java
+++ aulaMarco/Filme.java
@@ -1,11 +1,14 @@
 package aulaMarco;
 
 class Filme {
+
+    // atributos
     private String titulo;
     private int duracao;
     private int ano;
     private String sinopse;
 
+    // os metodos da classe
     public String getTitulacao() {
         return this.titulo;
     }
@@ -13,4 +16,25 @@ class Filme {
     public void setTitulacao(String titulo) {
         this.titulo = titulo;
     }
+
+    public int getDuracao() {
+        return this.duracao;
+    }
+
+    public void setDuracao(int duracao) {
+        this.duracao = duracao;
+    }
+
+    public int getAno() {
+        return this.ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public String getSinopse() {
+        return this.sinopse;
+    }
+
 }
--- aulaMarco/Principal.java
+++ aulaMarco/Principal.java
@@ -2,8 +2,18 @@ package aulaMarco;
 
 public class Principal {
     public static void main(String[] args) {
+
+
         Filme filme1 = new Filme();
+
         filme1.setTitulacao("Era do Gelo");
         System.out.println(filme1.getTitulacao());
+
+        filme1.setDuracao(30);
+        System.out.println(filme1.getDuracao());
+
+        filme1.setAno(2002);
+
+
     }   
 }
